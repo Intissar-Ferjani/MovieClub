5903ad91c37e5fbb5a26b6ecec51cab21d1687b5
Intissar-Ferjani <I.ferjani.26@gmail.com>
2024-11-10 15:07:29 +0100

    first commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/movie/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.movie.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.movie.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String API_KEY = "c1f46d17355ae2cb31a99e2888a39a48";
+}
